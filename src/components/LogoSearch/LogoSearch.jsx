7e9041197ca7c3854c9ccc40a69cf01fdcf337bc
Yash <kudveyash@gmail.com>
2023-05-24 11:21:03 +0530

    Initial Commit

---
+++ src/components/LogoSearch/LogoSearch.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './LogoSearch.css'
+
+const LogoSearch = () => {
+  return (
+    <div className='logoSearch'>
+      logoSearch
+    </div>
+  )
+}
+
+export default LogoSearch
