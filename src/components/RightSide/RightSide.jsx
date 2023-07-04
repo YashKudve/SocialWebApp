5a0fa75783f256d52f296315e2d8081d90b77a37
Yash <kudveyash@gmail.com>
2023-07-04 13:13:36 +0530

    New components added and necessary stylings added

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import './RightSide.css'
+import Home from "../../img/home.png"
+import Noti from '../../img/noti.png'
+import Comment from '../../img/comment.png'
+import {UilSetting} from '@iconscout/react-unicons'
+import {UilBell} from '@iconscout/react-unicons'
+import {UilComment} from '@iconscout/react-unicons'
+import {UilHome} from '@iconscout/react-unicons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faGear } from '@fortawesome/free-solid-svg-icons'
+
 
 const RightSide = () => {
   return (
-    <div>
+    <div className='RightSide'>
+      <div className="navIcons">
+        {/* <img src={Home} alt="" /> */}
+        <UilHome/>
+        {/* <FontAwesomeIcon icon={faGear} /> */}
+        <UilSetting />
+        <UilBell/>
+        <UilComment/>
+
+        {/* <img src={Noti} alt="" /> */}
+        {/* <img src={Comment } alt="" /> */}
+      </div>
       
+      <TrendCard/>
     </div>
-  )
+  ) 
 }
 
 export default RightSide
