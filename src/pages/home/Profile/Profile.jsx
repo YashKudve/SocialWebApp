783d2cfbba2746f8a414264225e7a37859e49fe1
Yash <kudveyash@gmail.com>
2023-07-19 11:40:22 +0530

    Adding the profile page

--- src/pages/home/Profile/Profile.jsx
+++ src/pages/home/Profile/Profile.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Profile.css'
+import ProfileLeft from '../../../components/ProfileLeft/ProfileLeft'
+
 const Profile = () => {
   return (
-    <div>
-      Profile
+    <div className='Profile'>
+      <ProfileLeft/>
     </div>
   )
 }
