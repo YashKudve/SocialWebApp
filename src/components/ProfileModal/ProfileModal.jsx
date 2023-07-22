bd0f1cfbe43ddb07e88bd29be2ffcd0347cbb6e9
Yash <kudveyash@gmail.com>
2023-07-22 17:20:21 +0530

    Modal update

--- src/components/ProfileModal/ProfileModal.jsx
+++ src/components/ProfileModal/ProfileModal.jsx
@@ -1,30 +1,56 @@
 import { useDisclosure } from '@mantine/hooks';
 import { Modal, useMantineTheme } from '@mantine/core';
 
-function ProfileModal() {
-  const [opened, { open, close }] = useDisclosure(false);
+function ProfileModal(modalOpened, setModalOpened) {
+//   const [opened, { open, close }] = useDisclosure(false);
   const theme = useMantineTheme();
 
   return (
     <>
       <Modal
-        opened={opened}
-        onClose={close}
+        // opened={opened}
+        // onClose={close}
         title="Authentication"
         overlayProps={{
           color: theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2],
           opacity: 0.55,
           blur: 3,
+          size: '55%',
+          opened: modalOpened,
+          onClose:()=>setModalOpened(false)
         }}
       >
        <form action="" className='infoForm'>
         <h3>Your Info</h3>
+
+        <div>
+          <input type="text" className="infoInput" name='FirstName' placeholder='First Name'/>
+          <input type="text" className="infoInput" name='LastName' placeholder='Last Name'/>
+          <input type="text" className="infoInput" name='worksAt' placeholder='Works At'/>
+        </div>
+
+        <div>
+          <input type="text" className="infoInput" name='livesIn' placeholder='Lives in'/>
+          <input type="text" className="infoInput" name='country' placeholder='Country'/>
+
+        </div>
+
+        <div>
+          <input type="text" name="" placeholder='Relationship Status'/>
+        </div>
+
+        <div>
+          Profile Image
+          <input type="file" name='profileImg' />
+          Cover Image
+          <input type="file" name="coverImg" />
+        </div>
        </form>
       </Modal>
 
-      <Group position="center">
+      {/* <Group position="center">
         <Button onClick={open}>Open modal</Button>
-      </Group>
+      </Group> */}
     </>
   );
 }
